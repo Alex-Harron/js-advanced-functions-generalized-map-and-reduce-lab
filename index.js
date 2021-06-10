279a05384a343160e06ea08442f58efee22150f6
Alex-Harron <aharron.harron@gmail.com>
2021-06-10 11:30:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+function map(arr, callback) {
+    const r = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        const theElement = arr[i];
+        r.push(callback(theElement));
+    }
+    return r
+}
+
+function reduce(arr, callback, start) {
+    let total;
+    if (start) {
+        total = start;
+        for (let i =0; i < arr.length; i++) {
+            total = callback(arr[i], total);
+        }
+        return total;
+    }else {
+        total = arr[0];
+        for (let i = 1; i < arr.length; i++) {
+            total = callback(arr[i], total);
+        }
+        return total; 
+    }
+}
